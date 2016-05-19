da89876843b50c7a6156debd93d7bd3bd4326720
wudan <pzdf@mac-mini.local>
2016-05-19 10:04:41 +0800

    Initial Commit

---
+++ C-Test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-Test
+//
+//  Created by PZDF on 16/5/19.
+//  Copyright © 2016年 PZDF. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
